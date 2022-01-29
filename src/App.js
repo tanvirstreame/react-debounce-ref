6f78b317e2f78dd481366fba829395e190836770
tanvirstreame <engr.tanvir.islam.1@gmail.com>
2022-01-29 10:13:42 +0600

    Added debounce and ref

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import './App.css';
 import { debounce } from "lodash";
 const axios = require('axios').default;
@@ -7,11 +7,37 @@ const axios = require('axios').default;
 function App() {
   const [search, setSearch] = useState(null);
   const [debounceSearch, setDebounceSearch] = useState(null);
+  const [searchResult, setSearchResult] = useState([]);
+  const nameInput = useRef(null);
+  const emailInput = useRef(null);
+  const passwordInput = useRef(null);
 
   const handleOnChange = (event) => {
     setSearch(event.target.value);
     setDebounceSearch(event.target.value);
   };
+
+
+ const handleSubmit = (e) => {
+ 
+ };
+
+ const handleFocus = (name) => {
+
+  if(name === "nameInput"){
+    nameInput.current.focus()
+  }
+  if(name === "emailInput"){
+    emailInput.current.focus()
+  }
+  if(name === "passwordInput"){
+    passwordInput.current.focus()
+  }
+  
+  console.log("ref",);
+ };
+
+ const handleReset = () => {};
 
   const searchApi = () => {
     if (debounceSearch !== null) {
@@ -19,6 +45,7 @@ function App() {
         .then(function (response) {
           // handle success
           console.log(response);
+          setSearchResult(response?.data?.data)
         })
         .catch(function (error) {
           // handle error
@@ -37,28 +64,28 @@ function App() {
 
   return (
     <React.Fragment>
-      <div>
+      <div className="container">
         <p>part 1</p>
         <label>
           Name:
-          <input placeholder="name" type="text" />
+          <input placeholder="name"  ref={nameInput}  type="text" />
         </label>
         <label>
           Email:
-          <input placeholder="email" type="text" />
+          <input placeholder="email" ref={emailInput} type="text" />
         </label>
 
 
         <label>
           Password:
-          <input placeholder="password" type="text" />
+          <input placeholder="password" ref={passwordInput} type="text" />
         </label>
         <hr />
-        <button>Focus Name Input</button>
-        <button>Focus Email Input</button>
-        <button>Focus Password Input</button>
+        <button onClick={() => handleFocus("nameInput")}>Focus Name Input</button>
+        <button onClick={() => handleFocus("emailInput")}>Focus Email Input</button>
+        <button  onClick={() => handleFocus("passwordInput")}>Focus Password Input</button>
         <hr />
-        <button>Submit</button>
+        <button onClick={() => handleSubmit()}>Submit</button>
         <button>Reset</button>
       </div>
       <div>
@@ -70,10 +97,23 @@ function App() {
             size="md"
             name="search"
             value={search}
+            placeholder={"Search with debounce"}
             onChange={(event) => handleOnChange(event)}
 
           />
         </label>
+      </div>
+      <div className="row">
+        {searchResult?.map((eachItem, key) => (
+          <div key={key} className="col-md-3 card">
+             <div>Id: {eachItem?.id}</div>
+            <div>Title: {eachItem?.title}</div>
+            <div>rating: {eachItem?.rating}</div>
+            <div>Username: {eachItem?.username}</div>
+          </div>
+        ))}
+
+        
       </div>
     </React.Fragment>
 
