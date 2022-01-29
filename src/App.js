326303c4a6516257dee11dff89daa864c9d90dcd
tanvirstreame <engr.tanvir.islam.1@gmail.com>
2022-01-29 09:47:04 +0600

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useCallback } from "react";
 import './App.css';
+import { debounce } from "lodash";
+const axios = require('axios').default;
+
 
 function App() {
+  const [search, setSearch] = useState(null);
+  const [debounceSearch, setDebounceSearch] = useState(null);
+
+  const handleOnChange = (event) => {
+    setSearch(event.target.value);
+    setDebounceSearch(event.target.value);
+  };
+
+  const searchApi = () => {
+    if (debounceSearch !== null) {
+      axios.get(`https://api.giphy.com/v1/gifs/search?api_key=290WqROqWOxuba8lbbhwktXdx9cDIMWA&q='${debounceSearch}`)
+        .then(function (response) {
+          // handle success
+          console.log(response);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+    }
+  };
+
+  const delayedQuery = useCallback(debounce(searchApi, 500), [debounceSearch]);
+
+
+  useEffect(() => {
+    delayedQuery();
+    return delayedQuery.cancel;
+  }, [debounceSearch, delayedQuery]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <div>
+        <p>part 1</p>
+        <label>
+          Name:
+          <input placeholder="name" type="text" />
+        </label>
+        <label>
+          Email:
+          <input placeholder="email" type="text" />
+        </label>
+
+
+        <label>
+          Password:
+          <input placeholder="password" type="text" />
+        </label>
+        <hr />
+        <button>Focus Name Input</button>
+        <button>Focus Email Input</button>
+        <button>Focus Password Input</button>
+        <hr />
+        <button>Submit</button>
+        <button>Reset</button>
+      </div>
+      <div>
+        <hr />
+        <p>part 2</p>
+        <label>
+          Search:
+          <input
+            size="md"
+            name="search"
+            value={search}
+            onChange={(event) => handleOnChange(event)}
+
+          />
+        </label>
+      </div>
+    </React.Fragment>
+
+  )
 }
 
 export default App;
